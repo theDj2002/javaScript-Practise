30ed796b064e53694fb619d07bb509557509ab40
dibyajyoti.dj <dibyajyoti.s@iserveu.co.in>
2025-01-08 23:11:18 +0530

    Day-1 of java Script

---
+++ 02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all JS code as newer Version
+
+// alert(3+3) we are using node js not browser.
+console.log(3+3)
+ 
+//Always maintain Code readability
+
+let name="Sunidhi"
+let age=23
+let married=false
+// housewife=null
+// location;
+
+//number 2 to the power 53
+//String
+//boolean
+//null => standalone value
+//undefined
+//symbol => unique
+
+//object
+ console.log(typeof married) //boolean
+ console.log(typeof undefined) //undefined
+ console.log(typeof null) //object
+ 
---
+++ constants.js
@@ -0,0 +1,16 @@
+const accountId=2233
+var name="sunidhi"
+let title="Chattar"
+let accountState;
+ number=998877
+
+ /*
+ prefer not to use var 
+ because of issue in scope and functional scope
+ */
+//  accountId=1122
+ title="Sahoo"
+number=2233
+ console.table([title,number,accountState])
+//  console.log(accountId);
+ 
