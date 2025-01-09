638e7792559bb93401a730327f33dfadc1d14850
dibyajyoti.dj <dibyajyoti.s@iserveu.co.in>
2025-01-10 00:03:07 +0530

    Day-2 java Script

---
+++ 03_dataTypeConv.js
@@ -0,0 +1,68 @@
+let score=33
+let card="33"
+
+// console.log(typeof score)
+// console.log(typeof(card))
+let value=null
+let truth=false
+let bikes=undefined
+
+let valueInNumber=Number(bikes)
+// console.log(valueInNumber)
+
+//33=> number
+//String and undefined => NaN
+//boolean true=>1 and false=> 0
+//null => 0
+//"33Abc"=> NaN
+
+let isLogged=0
+ let convertValue= Boolean(isLogged)
+//  console.log(convertValue)
+
+ //String => true
+ //Number => True
+//0 => false
+//1 => True
+
+let object=null
+let convertedString= String(object)
+
+// console.log(convertedString)
+// console.log(typeof convertedString)
+
+/***************************************Operations***************************************************/
+                                   
+// console.log(2+2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+let firstNM="Sunidhi"
+let lastNm="Sahoo"
+let fullName=firstNM+lastNm
+
+// console.log(fullName)
+// console.log("1" + 2)
+// console.log("1" + "2")
+// console.log("1" + 2 + 3)
+// console.log(1 + 2 + "3")
+
+// console.log(+true)
+// console.log(+"")
+
+let gameCounter=100
+++gameCounter
+// console.log(gameCounter);
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
---
+++ 04_comparison.js
@@ -0,0 +1,16 @@
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null >= 0);
+
+// console.log(undefined >0);
+// console.log(undefined ==0);
+// console.log(undefined >=0);
+
+//===  it compares also the Data types 
+
+console.log("2" === 2);
+
+
+
+
+
